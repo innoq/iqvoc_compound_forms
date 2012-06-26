ee1fb5c43218b71e5f14858150a2a168535398e4
Robert Glaser <mail@robert-glaser.de>
2012-06-26 16:17:20 +0200

    /tabs/spaces/

--- app/assets/javascripts/iqvoc_compound_forms/compound_forms.js
+++ app/assets/javascripts/iqvoc_compound_forms/compound_forms.js
@@ -2,21 +2,21 @@
 /*global jQuery, IQVOC */
 
 jQuery(document).ready(function($) {
-	"use strict";
+  "use strict";
 
-	$("input[type=button].add-compound-form").live("click", function(ev) {
-		var original = $(this).closest("li");
-		var clone = original.clone();
-		var container = $("div.entity_select", clone);
-		var el = $("input.entity_select", clone).
-			attr("data-entities", "").val("").insertBefore(container);
-		container.remove();
-		clone.insertAfter(original);
-		new IQVOC.EntitySelector(el[0]);
-	});
+  $("input[type=button].add-compound-form").live("click", function(ev) {
+    var original = $(this).closest("li");
+    var clone = original.clone();
+    var container = $("div.entity_select", clone);
+    var el = $("input.entity_select", clone).
+      attr("data-entities", "").val("").insertBefore(container);
+    container.remove();
+    clone.insertAfter(original);
+    new IQVOC.EntitySelector(el[0]);
+  });
 
-	$("input[type=button].rm-compound-form").click(function(ev) {
-		// Remove the <li> that contains the widget
-		$(this).parent().remove();
-	});
+  $("input[type=button].rm-compound-form").click(function(ev) {
+    // Remove the <li> that contains the widget
+    $(this).parent().remove();
+  });
 });
