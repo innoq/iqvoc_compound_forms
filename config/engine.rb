65a2ca3141cc05c8f714a0e44d613a93a3c11ef5
Robert Glaser <mail@robert-glaser.de>
2012-11-20 10:36:15 +0100

    simplified engine migrations

--- config/engine.rb
+++ config/engine.rb
@@ -4,9 +4,11 @@ module Iqvoc
   module CompoundForms
 
     class Engine < Rails::Engine
-
       paths["lib/tasks"] << "lib/engine_tasks"
 
+      initializer "iqvoc_compound_forms.load_migrations" do |app|
+        app.config.paths['db/migrate'] += Iqvoc::CompoundForms::Engine.paths['db/migrate'].existent
+      end
     end
 
   end
