c95306508b22296b9e56bbcfcabda237c9d41766
Marc Jansing <marc.jansing@innoq.com>
2016-05-18 13:02:30 +0200

    fix compound form javascript widget

--- app/assets/javascripts/iqvoc_compound_forms/compound_forms.js
+++ app/assets/javascripts/iqvoc_compound_forms/compound_forms.js
@@ -6,17 +6,23 @@ jQuery(document).ready(function($) {
 
   $("input[type=button].add-compound-form").on("click", function(ev) {
     var original = $(this).closest(".compound-form");
-    var clone = original.clone();
+    var clone = original.clone(true);
     var container = $("div.entity_select", clone);
     var el = $("input.entity_select", clone).
       attr("data-entities", "").val("").insertBefore(container);
     container.remove();
     clone.insertAfter(original);
     new IQVOC.EntitySelector(el[0]);
+
+    $("ul.entity_list", clone).html("");
   });
 
   $("input[type=button].rm-compound-form").click(function(ev) {
-    // Remove the <li> that contains the widget
-    $(this).parent().remove();
+    // clear compound form values
+    $(this).parent().find('input.entity_select').val("");
+
+    // hide the <li> that contains the widget
+    // don't drop it to keep cleared form inputs
+    $(this).parent().parent().hide();
   });
 });
