7d891f2530a93dbe9bc9d409e6436c348d7e8913
Marc Jansing <marc.jansing@innoq.com>
2021-01-13 15:47:47 +0100

    bundle using faucet

--- app/assets/javascripts/manifest.js
+++ app/assets/javascripts/manifest.js
@@ -1,4 +1,4 @@
-//= require framework
-//= require iqvoc/manifest
-//= require iqvoc_skosxl/manifest
-//= require iqvoc_compound_forms/manifest
+import '../../../node_modules/iqvoc/app/assets/javascripts/framework';
+import '../../../node_modules/iqvoc/app/assets/javascripts/iqvoc/manifest';
+import '../../../node_modules/iqvoc_skosxl/app/assets/javascripts/iqvoc_skosxl/manifest';
+import './iqvoc_compound_forms/manifest';
